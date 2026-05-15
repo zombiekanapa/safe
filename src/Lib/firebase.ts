8933aff994d56de638db865d010ad828c033b6e0
Szymon Karpierz <80419458+zombiekanapa@users.noreply.github.com>
2026-05-15 13:55:25 +0200

    Create firebase.ts

---
+++ src/Lib/firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import firebaseConfig from '../../firebase-applet-config.json';
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app, (firebaseConfig as any).firestoreDatabaseId);
+
+export default app;
